3ae2d8ad47130cd093b85cecb73125e23d7edb47
Ellie <ellierparry@gmail.com>
2024-08-07 15:51:08 +0100

    Ticket 6 started.

--- users/users.test.js
+++ users/users.test.js
@@ -2,6 +2,7 @@ import { test, expect } from 'vitest';
 import request from 'supertest';
 
 import app from '../app.js';
+import { resetUsersTable } from '../db/helpers.js'
 
 
 test('Make sure GET /api/health works', async () => {
@@ -16,3 +17,25 @@ test('Make sure GET /api/health works', async () => {
     expect(response.status).toEqual(200);
 
 });
+
+
+// write a test skeleton with a descriptive test name ("GET /api/users" could be a starting point)
+// run tests to make sure the skeleton passes on its own
+// then within the test:
+//    ARRANGE:
+//      use the `resetUsersTable` function to reset the database table to a known state
+//    ACT:
+//      use Supertest to send a GET request to the `/api/users` endpoint
+//      wait for the response
+//    ASSERT:
+//      assert that the response body is an object
+//      assert that response body.success is true
+//      assert that response body.payload is an array
+//      loop over the payload array. for each user object in the payload array:
+//          assert that user object.id is a number
+//          assert that user object.username is a string
+//      assert that the response status code is 200
+//      assert that there's a Content-Type response header which contains `application/json`
+//      any other assertions that you think would be useful
+// run tests to ensure they passes
+// temporarily break the implementation in `users/users.controller.js` to ensure test fails and then change back so that tests pass
