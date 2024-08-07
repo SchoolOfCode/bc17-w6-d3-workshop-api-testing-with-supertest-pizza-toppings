da43ef51cd28b69979ba97df31d471edd39b91ca
Ellie <ellierparry@gmail.com>
2024-08-07 16:50:35 +0100

    Get request test made.

--- users/users.test.js
+++ users/users.test.js
@@ -15,14 +15,13 @@ test("Make sure GET /api/health works", async () => {
   expect(response.status).toEqual(200);
 });
 
-test("Make sure GET /api/users", async () => {});
 
-// write a test skeleton with a descriptive test name ("GET /api/users" could be a starting point)
-// run tests to make sure the skeleton passes on its own
+// write a test skeleton with a descriptive test name ("GET /api/users" could be a starting point) ✔
+// run tests to make sure the skeleton passes on its own ✔
 // then within the test:
-//    ARRANGE:
+//    ARRANGE: ✔
 //      use the `resetUsersTable` function to reset the database table to a known state
-//    ACT:
+//    ACT: ✔
 //      use Supertest to send a GET request to the `/api/users` endpoint
 //      wait for the response
 //    ASSERT:
@@ -37,3 +36,26 @@ test("Make sure GET /api/users", async () => {});
 //      any other assertions that you think would be useful
 // run tests to ensure they passes
 // temporarily break the implementation in `users/users.controller.js` to ensure test fails and then change back so that tests pass
+
+test("Make sure GET /api/users", async () => {
+
+    // Arrange
+    await resetUsersTable();
+
+    // Act
+    const response = await request(app).get("/api/users");
+
+    // Assert
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.success).toBe(true);
+    expect(response.body.payload).toBeInstanceOf(Array);
+
+    response.body.payload.forEach(user => {
+        expect(typeof user.id).toBe('number');
+        expect(typeof user.username).toBe('string');
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toMatch(/application\/json/);
+
+});
